92f203fba3bd794d76e37f978a900db2ec55b302
Ivica Ceraj <ceraj@mit.edu>
2013-04-24 17:16:09 -0400

    Adding StarORF loader w/ info.

--- src/StarX/StarORF.js
+++ src/StarX/StarORF.js
@@ -1,9 +1,10 @@
-define( ['StarORF/main'],function(StarORF) {
-	return {
-		configure: function(config) {
-            StarORF.configure(config);
-			//var html = "<iframe src='http://star.mit.edu/media/uploads/star/html/orf/app.html' style='width:800px;height:800px; border:0px;overflow:none'></iframe>";
-			//$('#'+config.element_id).html( html ) ;
-		}
-	}
+define(['StarORF/main'], function (StarORF) {
+    return {
+        configure: function (config) {
+            $('#' + config.element_id).html(StarORF ? "Loading StarORF" : "Failed initializing StarORF");
+            if (StarORF) {
+                StarORF.configure(config);
+            }
+        }
+    }
 });
