5bbf30bfd34286559ff6883b04a1cfd4ce344b04
Ivica Ceraj <iceraj@gmail.com>
2013-06-21 13:34:13 -0400

    Typescript Library jQuery Declaration

---
+++ src/StarX/lib/jquery.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../../starx/src/StarX/lib/jquery.d.ts" />
+
+declare var $:JQueryStatic;
+declare var $:JQueryStatic;
+
+import jQuery = module("jquery");
+declare var exports;
+exports.$ = jQuery;
+jQuery['noConflict'](true);
+
+export class version {
+    constructor() {
+        console.info(jQuery['fn'].jquery);
+    }
+};
