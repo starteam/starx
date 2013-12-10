7556754c174fd64301c425d784f431b6abd5d23c
Ivica Ceraj <iceraj@gmail.com>
2013-12-10 14:43:05 -0500

    Cleanup underscore import

--- src/StarX/lib/underscore.js
+++ src/StarX/lib/underscore.js
@@ -3,32 +3,7 @@
 /// <amd-dependency path="StarX/underscore" />
 define(["require", "exports", "StarX/underscore"], function(require, exports) {
     
-    var _backup = window;
-
-    //
-    var underscore = require(['StarX/underscore']);
-    console.info("underscore is" + underscore);
-    console.info(underscore);
-    if (underscore) {
-        console.info("StarX underscore");
-    }
     exports._ = window['_'];
-    var _ = exports._;
-    _.each([1, 2, 3], function (e) {
-        console.info(e);
-    });
-    _.noConflict();
-
-    (function (__) {
-        var version = (function () {
-            function version() {
-            }
-            version.prototype.to = function () {
-            };
-            return version;
-        })();
-        __.version = version;
-    })(exports.__ || (exports.__ = {}));
-    var __ = exports.__;
+    window['_'].noConflict();
 });
 //# sourceMappingURL=underscore.js.map
--- src/StarX/lib/underscore.ts
+++ src/StarX/lib/underscore.ts
@@ -3,25 +3,6 @@
 /// <amd-dependency path="StarX/underscore" />
 
 declare var exports;
-var _backup = window;
-//
-var underscore = require(['StarX/underscore']);
-console.info("underscore is" + underscore);
-console.info(underscore);
-if (underscore) {
-    console.info("StarX underscore");
-}
-exports._ = window['_'];
-var _ = exports._;
-_.each([1, 2, 3], function (e) {
-    console.info(e)
-});
-_.noConflict();
-
-export module __ {
-    export class version {
-        to():any {
-        }
-    }
-}
+export var _ = window['_'];
+window['_'].noConflict();
 
