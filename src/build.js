8b3397106484c84779985632060065aa734e3165
Ivica Ceraj <iceraj@gmail.com>
2014-01-09 16:39:12 -0500

    Working on using r.js

---
+++ src/build.js
@@ -0,0 +1,8 @@
+({
+	buildURL: ".",
+	name:'StarX/main',
+	paths: {
+		'jquery': 'StarX/lib/jquery-1.10.1.min'
+	},
+	out: 'main.min.js'
+})
---
+++ src/build_all.js
@@ -0,0 +1,24 @@
+({
+	appDir: ".",
+	baseUrl: ".",
+	dir: "../build",
+	modules: [ 
+//		{ name: "StarX/main" },
+	//	{ name: "StarGenetics/main" },
+	//	{ name: "StarSimpleText/main" },
+	//	{ name: "StarTMI/main" },
+		{ name: "StarDistanceMap/main" },
+	] ,
+	paths: {
+	        'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min',
+//		'jquery': 'StarX/lib/jquery-1.10.1.min',
+		'lib/soyutils': 'StarX/lib/soyutils',
+		'jquery-ui': 'StarX/lib/jquery-1.10.3.ui.min',
+		'css': 'StarX/css'
+	},
+	map: {
+                '*': {
+                    'css': 'StarX/css'
+                }
+}
+})
