ff3ecfca5a4b798f46505c18edc83f2ae93cab90
Ivica Ceraj <iceraj@gmail.com>
2014-01-09 16:38:17 -0500

    Soy file declaration

---
+++ src/StarDistanceMap/widget.soy.d.ts
@@ -0,0 +1,8 @@
+declare module "StarDistanceMap/widget.soy" {
+    function widget(state:any):any;
+
+    function gene_distance(state:any):any;
+}
+
+declare function widget(state:any):any;
+declare function gene_distance(state:any):any;
