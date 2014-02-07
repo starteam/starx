b1f4f8b76429ff909c0e283a877680588adb8b0c
Ivica Ceraj <iceraj@gmail.com>
2014-02-07 15:59:54 -0500

    StarStudentStateExport

--- src/StarStudentStateExport/main.soy.js
+++ src/StarStudentStateExport/main.soy.js
@@ -13,7 +13,7 @@ define(['require','exports','lib/soyutils'],function(require,exports,soy){
  */
 ssse.main = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append(soy.$$escapeHtml(opt_data.config), '<form action="#" method="POST" enctype="multipart/form-data"><span class="fileinput-button"><span>Add student_state files...</span><input type="file" class="student_files" name="student_files" multiple></span><br><span class="fileinput-button"><span>Add mapping file...</span><input type="file" class="mapping_file" name="mapping_file"></span><br><input type="text" class="mapping_prefix" name="mapping_prefix"><button type="submit" class="start">Start coversion</button><button type="reset" class="cancel">Reset</button><button type="button" class="select_table">Select Table</button><span class="status"></span><span class="out"></span></form>');
+  output.append('<form action="#" method="POST" enctype="multipart/form-data"><span class="fileinput-button"><span>Add student_state files...</span><input type="file" class="btn btn-default student_files active " name="student_files" multiple></span><br><span class="fileinput-button"><span>Add mapping file...</span><input type="file" class="btn btn-default mapping_file active" name="mapping_file"></span><br><span class="fileinput-button"><span>Prefix:</span><input type="text"class="mapping_prefix" name="mapping_prefix"></span><br><p class="fileinput-button"><button type="submit" class="btn btn-primary start">Start coversion</button><button type="reset" class="btn btn-danger cancel">Reset</button><button type="button" class="btn btn-info select_table">Select Table</button></p><span class="status"></span><span class="out"></span></form>');
   return opt_sb ? '' : output.toString();
 };
 
@@ -26,14 +26,14 @@ ssse.main = function(opt_data, opt_sb) {
  */
 ssse.table = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('<table id="csv_table">');
-  var rowList7 = opt_data.rows;
-  var rowListLen7 = rowList7.length;
-  for (var rowIndex7 = 0; rowIndex7 < rowListLen7; rowIndex7++) {
-    var rowData7 = rowList7[rowIndex7];
-    output.append('<tr><td>', soy.$$escapeHtml(rowData7[0]), '</td><td>', soy.$$escapeHtml(rowData7[1]), '</td><td>', soy.$$escapeHtml(rowData7[2]), '</td></tr>');
+  output.append('<h2>Converted data</h2><table id="csv_table" class="table table-striped"><tbody>');
+  var rowList6 = opt_data.rows;
+  var rowListLen6 = rowList6.length;
+  for (var rowIndex6 = 0; rowIndex6 < rowListLen6; rowIndex6++) {
+    var rowData6 = rowList6[rowIndex6];
+    output.append('<tr><td>', soy.$$escapeHtml(rowData6[0]), '</td><td>', soy.$$escapeHtml(rowData6[1]), '</td><td>', soy.$$escapeHtml(rowData6[2]), '</td></tr>');
   }
-  output.append('</table>');
+  output.append('</tbody></table>');
   return opt_sb ? '' : output.toString();
 };
 for(var i in ssse) { exports[i] = ssse[i] };
