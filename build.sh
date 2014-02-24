81d5d4331e1ce2c5dc560d049ba30efaf22c7117
Ivica Ceraj <iceraj@gmail.com>
2014-02-24 16:39:18 -0500

    Tutorial

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 r.js -o src/build_all.js
 cat src/demo_loader.prefix build/SGC.js src/demo_loader.suffix > src/SGC.min.js
-cp src/SGC.min.js src/StarCompiled/V20140225_EX5/SGC.min.js
+#cp src/SGC.min.js src/StarCompiled/V20140225_EX5/SGC.min.js
+cp src/SGC.min.js src/StarCompiled/V20140225_TUTORIAL/SGC.min.js
