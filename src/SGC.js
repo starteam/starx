f6468475fad87eb4b272c9d06730855f3c83da14
Ivica Ceraj <iceraj@gmail.com>
2014-02-26 16:10:08 -0500

    Build stuff

--- src/SGC.js
+++ src/SGC.js
@@ -1,3 +1,60 @@
+(function(window,undefined){
+
+var modules = {} ;
+modules['jquery'] = window.$;
+var require = function(deps, creator) {
+    var args = [];
+    var global = { window:window };
+    var exports = {};
+    for( var i in deps )
+    {
+        var m = deps[i];
+        if( m == 'exports')
+        {
+            args.push(exports);
+        }
+        else if( modules[m])
+        {
+            args.push(modules[m]);
+        } else if(m.indexOf( "../") == 0 )
+        {
+            var mm = m.substr(3);
+            if( modules[mm])
+            {
+                args.push(modules[mm]);
+            }
+        }
+    }
+    console.info( args );
+    creator.apply( global , args );
+}
+    modules['require'] = require;
+
+var define = function(what, deps, creator) {
+
+    creator = creator || Object;
+    var args = [];
+    var creator_this = { window: window};
+    var exports = {};
+    for( var i in deps )
+    {
+        var m = deps[i];
+        if( m == 'exports')
+        {
+            args.push(exports);
+        }
+        else
+        {
+            console.info( "modules" + modules ) ;
+            args.push(modules[m]);
+        }
+    }
+    console.info( "define start " + what);
+    var module = creator.apply( creator_this , args );
+    modules[what] = exports;
+    console.info( "defined end  " + what);
+}
+    // here goes content
 define('SGC',['StarX/main', 'StarGenetics/main'], function ( main, simple) {
     var parent = document.getElementById("insert_here");
 //    parent.textContent = '{["StarX":"StarGenetics", "gwt_path":"StarCompiled/V20140225_EX5/gwtframe.html","state":"star_genetics_input","width":500, "height":500,"color":"red","Widget":"JSApp","config":{"model_type":"bundled_samples","bundled_samples":"fruit_fly_exercise_5"}]}';
@@ -6,3 +63,6 @@ define('SGC',['StarX/main', 'StarGenetics/main'], function ( main, simple) {
     console.info( "Hello");
 });
 require(['SGC'], function(q) { console.info(q); } ) ;
+    // here goes content
+
+})(window);
--- src/build_all.js
+++ src/build_all.js
@@ -1,4 +1,15 @@
 ({
+	onBuildWrite: function( moduleName, path, contents) {
+		console.info( "writing " , moduleName , path ); 
+//		var prefix = "(function(window,undefined){ var modules = {} ; modules['jquery'] = window.$; var require = function(deps, creator) { var args = []; var global = { window:window }; var exports = {}; for( var i in deps ) { var m = deps[i]; if( m == 'exports') { args.push(exports); } else if( modules[m]) { args.push(modules[m]); } else if(m.indexOf( '../') == 0 ) { var mm = m.substr(3); if( modules[mm]) { args.push(modules[mm]); } } } console.info( args ); creator.apply( global , args ); }; modules['require'] = require; var define = function(what, deps, creator) { creator = creator || Object; var args = []; var creator_this = { window: window}; var exports = {}; for( var i in deps ) { var m = deps[i]; if( m == 'exports') { args.push(exports); } else { console.info( 'modules' + modules ) ; args.push(modules[m]); } } console.info( 'define start ' + what); var module = creator.apply( creator_this , args ); modules[what] = exports; console.info( 'defined end  ' + what); };";
+//		var suffix = "})(window);";
+		var prefix = '' ;
+		var suffix = '' ;
+		return prefix + contents + suffix ;
+	},
+	optimize: "none",
+	preserveLicenseComments:false,
+	generateSourceMaps: true,
 	appDir: ".",
 	baseUrl: ".",
 	dir: "../build",
@@ -20,6 +31,8 @@
 	map: {
                 '*': {
                     'css': 'StarX/css'
-                }
+                },
+	logLevel: 5
+	
 }
 })
