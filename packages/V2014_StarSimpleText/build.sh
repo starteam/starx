d8557f4a2ce2c6b8bc019ba23c94e1c2fac83995
Ivica Ceraj <iceraj@gmail.com>
2014-04-25 14:27:38 -0400

    Remove source mapping

--- packages/V2014_StarSimpleText/build.sh
+++ packages/V2014_StarSimpleText/build.sh
@@ -11,6 +11,6 @@ cp $PKG_DIR/main.js prebuild/main.js
 cp $PKG_DIR/build.js prebuild/build.js
 
 r.js -o prebuild/build.js
-cat prebuild/demo_loader.prefix build/main.js prebuild/demo_loader.suffix > prebuild/main.min.js
+cat prebuild/demo_loader.prefix build/main.js prebuild/demo_loader.suffix | grep -v "^//\# sourceMappingURL" > prebuild/main.min.js
 cp $PKG_DIR/index.html $OUT_DIR
 cp prebuild/main.min.js $OUT_DIR
